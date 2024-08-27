8a6788b0ccf64b8b8662f8c7a21e5d33cc4b5fdd
NiklasBaer <oz3angam3r@gmail.com>
2024-08-27 22:32:57 +0200

    Beschreibung

--- Learn/Try01/main.py
+++ Learn/Try01/main.py
@@ -1,38 +1,37 @@
+
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
-
 nnfs.init()
-
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-X, Y = spiral_data(100,3)
+                                                                    #* import von Modulen und Datensetzt
+                                                              
+X, Y = spiral_data(100,3)                                           #* Test daten erstellen dur eine Spiralfunktion 
 
 
-class Layer_Dense:
+class Layer_Dense:                                                  #? Klasse Layer erstellen
     
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+        
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)  #* festlegen wie viele Inputs und Neuroen wir haben
+
+        self.biases = np.zeros((1, n_neurons))                      #* biases alles = 0
     
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
+        self.output = np.dot(inputs, self.weights) + self.biases    #* brechnet den Output jedes einzelnen Neurones
         
-class Activatiopn_ReLU:
+class Activatiopn_ReLU:                                             #? Klasse Activtion erstellen hier legen wir fest wann jedes Einzellen Neuron activ ist oder nicht mit ein ReLU
     
     def forward(self, inputs):
         
-        self.output = np.maximum(0, inputs)
+        self.output = np.maximum(0, inputs)                         #* Alles werste die unter 0 liegen werden auf eine 0 gesetz
    
    
    
      
-layer1 = Layer_Dense(2,5)
-activation1 = Activatiopn_ReLU()
+layer1 = Layer_Dense(2,5)                                           #* Erstellung des Objektes Layer1 mit 2 inputs und 5 Neuronen
+activation1 = Activatiopn_ReLU()                                    #* Ertsellung des Objektes activation1
 
-layer1.forward(X)
-activation1.forward(layer1.output)
+layer1.forward(X)                                                   #* Layer1 mit der Funktion forward ausführen und mit den Argumenten X (input wertde)
+activation1.forward(layer1.output)                                  #* Nun alles werte unter 0 auf 0 setzten
 
-print(activation1.output)
+print(activation1.output)                                           # * outputs ausgeben auf der Console
